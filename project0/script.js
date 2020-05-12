92a345158bbb08a513649be96e62a734712a8099
Dave Genest <drwgenest@gmail.com>
2020-05-12 07:58:31 -0700

    Todo script

--- project0/script.js
+++ project0/script.js
@@ -1,120 +1,128 @@
-const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
-}
 
-const STATUS = {
-  ACTIVE: 'active',
-  COMPLETED: 'completed'
-}
+const itemCountContainer = document.getElementById('item-count')
+const uncheckedCountContainer = document.getElementById('unchecked-count')
+const todoList = document.getElementById('todo-list')
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
-let nodes = []
-// Prompt user for Todo description
-// Creates a new Todo object and appends to the list
-// Updates the counters
-function newTodo() {
-  const result = window.prompt('Enter ToDo description')
-  if (result !== null && result.length > 0) {
-    const newTodo = new Todo(result)
-    list.appendChild(newTodo.getListItem())
-    nodes.push(newTodo)
-    updateCounts()
-  }
-}
-
-// Removes requested node from todo list element, updates counters
-// @param node = an html element
-function deleteTodo(node) {
-  list.removeChild(node)
-  updateCounts()
-}
-
-// Resets the counts based on item status
-function updateCounts() {
-  itemCountSpan.innerHTML = nodes.length
-  uncheckedCount = nodes.filter(node => node.status === STATUS.ACTIVE).length
-  uncheckedCountSpan.innerHTML = uncheckedCount
-}
-
-// JS Class representing a todo as an HTML list item
 class Todo {
-  // @param String - description entered by user
-  constructor(desc) {
+  constructor(description, deleteHandler, checkHandler) {
     this.id = Date.now()
-    this.node = this.createNewNode(desc)
-    this.status = STATUS.ACTIVE
+    this.task = description
+    this.node = this.createNode(this.id, this.task, deleteHandler, checkHandler)
+    this.checked = false
   }
 
-  // creates a new HTML element with the format:
-  // <li>
-  //   <span>
-  //     <input type='checkbox' />
-  //     <span>{description}</span>
-  //     <button>Delete</button>
-  //   </span>
-  // </li>
-  createNewNode(text) {
-    const node = document.createElement('li')
-    node.setAttribute('class', classNames.TODO_ITEM)
-    node.setAttribute('id', this.id)
-    
-    const todoContent = document.createTextNode(text)
-    
-    const newTodoCheckbox = document.createElement('input')
-    newTodoCheckbox.setAttribute('type', 'checkbox')
-    newTodoCheckbox.setAttribute('class', classNames.TODO_CHECKBOX)
-    newTodoCheckbox.addEventListener('change', this.handleCheck.bind(this))
-    
-    const newTodoTextContainer = document.createElement('span')
-    newTodoTextContainer.setAttribute('class', classNames.TODO_TEXT)
-    newTodoTextContainer.appendChild(todoContent)
-    
-    const todoButtonContainer = document.createElement('span')
-    const todoButton = document.createElement('button')
-    const todoButtonText = document.createTextNode('Delete')
-    todoButton.setAttribute('class', 'todo-delete')
-    todoButton.addEventListener('click', this.handleDelete.bind(this))
-    todoButton.appendChild(todoButtonText)
-    todoButtonContainer.appendChild(todoButton)
-
-    const newTodoSpan = document.createElement('span')
-    newTodoSpan.appendChild(newTodoCheckbox)
-    newTodoSpan.appendChild(newTodoTextContainer)
-    newTodoSpan.appendChild(todoButton)
-
-    node.appendChild(newTodoSpan)
-    return node
-  }
-
-  // @param evt - event from the checkbox
-  // Updates the classlist of the node that was (un)checked
-  handleCheck(evt) {
-    if (evt.target.checked) {
-      this.status = STATUS.COMPLETED
-      this.node.classList.add('todo-completed')
-    } else {
-      this.status = STATUS.ACTIVE
-      this.node.classList.remove('todo-completed')
-    }
-    updateCounts()
-  }
-
-  // Deletes the todo that was clicked by calling the page
-  // function delete with the ndode
-  handleDelete() {
-    nodes = nodes.filter(node => node.id !== this.id)
-    deleteTodo(this.node)
-  }
-
-  // Returns the <li> node for this todo
-  getListItem() {
+  getNode() {
     return this.node
   }
+
+  toggleCheckbox(val) {
+    this.checked = val
+  }
+
+  // creates a <li> node like:
+  // <li>
+  //   <checkbox>
+  //   <text>
+  //   <delete>
+  // </li>
+  createNode(id, text, deleteHandler, checkHandler) {
+    const listItem = document.createElement('li')
+    listItem.setAttribute('id', id)
+    
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.addEventListener('change', (e) => { this.toggleCheckbox(e.target.checked); checkHandler(id) })
+
+    const textNode = document.createElement('span')
+    textNode.appendChild(document.createTextNode(text))
+    
+    const deleteButton = document.createElement('button')
+    deleteButton.appendChild(document.createTextNode('Delete'))
+    deleteButton.addEventListener('click', function() {deleteHandler(id)})
+
+    listItem.appendChild(checkbox)
+    listItem.appendChild(deleteButton)
+    listItem.appendChild(textNode)
+
+    return listItem
+  }
 }
 
+class TodoList {
+  constructor() {
+    this.todos = []
+  }
 
+  addTodo(item) {
+    this.todos.push(item)
+    this.renderList()
+  }
+
+  removeTodo = (id) => {
+    this.todos = this.todos.filter(todo => todo.id !== id)
+    const child = document.getElementById(id)
+    todoList.removeChild(child)
+    itemCountContainer.innerHTML = this.todos.length
+    uncheckedCountContainer.innerHTML = this.todos.filter(todo => todo.checked).length
+  }
+
+  handleCheck = id => {
+    uncheckedCountContainer.innerHTML = this.todos.filter(todo => todo.checked).length
+  }
+
+  renderList() {
+    this.todos.forEach(todo => todoList.appendChild(todo.getNode()))
+    itemCountContainer.innerHTML = this.todos.length
+    uncheckedCountContainer.innerHTML = this.todos.filter(todo => todo.checked).length
+  }
+}
+
+const myTodos = new TodoList()
+
+function newTodo() {
+  const description = prompt("Enter Todo Description:")
+  const newTodo = new Todo(description, myTodos.removeTodo, myTodos.handleCheck)
+  myTodos.addTodo(newTodo)
+  myTodos.renderList
+}
+
+const throttle = function(fn, delay) {
+  let throttled = false
+  return function() {
+    if (throttled)
+      return
+
+    throttled = setTimeout(() => throttled = false, delay)
+    fn()
+  }
+}
+
+const debounce = function(fn, delay) {
+  let timer
+  return function() {
+    if (timer) {
+      clearTimeout(timer)
+    } 
+    timer = setTimeout(fn, delay)
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+  const imageList = document.querySelectorAll('.lazy')
+
+  const ImageObserver = new IntersectionObserver(function(entries) {   
+    console.log('imageobserver callback')
+    entries.forEach(element => {
+      if (element.isIntersecting) {
+        const image = element.target
+        image.src = image.dataset.src
+        image.classList.remove('lazy')
+        ImageObserver.unobserve(image)
+      } 
+    }) 
+  })
+
+  imageList.forEach(image => ImageObserver.observe(image))
+})
+
+window.addEventListener('resize', debounce(() => console.log('here'), 1000))
